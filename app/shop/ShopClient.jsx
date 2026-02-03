67d46e535503bdb621aa956cc2aadba14e73bc99
parthrugved <parthrugved@gmail.com>
2026-02-03 21:50:24 +0530

    catogary commit

--- app/shop/ShopClient.jsx
+++ app/shop/ShopClient.jsx
@@ -12,107 +12,107 @@ export default function ShopClient() {
                 <section>
                     <ul>
                         <li>
-                            <details>
-                                <summary>Casual Wear</summary>
-                                <section className="text-gray-600 body-font">
-                                    <div className="container px-5 py-24 mx-auto">
-                                        <div id="product-container-casual" className="flex flex-wrap -m-4">
 
-                                        </div>
+                            {/* Casual Wear */}
+                            <section className="text-gray-600 body-font">
+                                <div className="container px-5 py-24 mx-auto">
+                                    <div id="product-container-casual" className="flex flex-wrap -m-4">
+
                                     </div>
+                                </div>
 
-                                </section>
-                                <Script src="/products/casual_wear.js" strategy="afterInteractive" />
-                            </details>
+                            </section>
+                            <Script src="/products/casual_wear.js" strategy="afterInteractive" />
+
                         </li>
 
                         <li>
-                            <details>
-                                <summary>Cotton Sarees</summary>
-                                <section className="text-gray-600 body-font">
-                                    <div className="container px-5 py-24 mx-auto">
-                                        <div id="product-container-cotton" className="flex flex-wrap -m-4">
 
-                                        </div>
+                            {/* Cotton Sarees */}
+                            <section className="text-gray-600 body-font">
+                                <div className="container px-5 py-24 mx-auto">
+                                    <div id="product-container-cotton" className="flex flex-wrap -m-4">
+
                                     </div>
+                                </div>
 
-                                </section>
-                                <Script src="/products/cotton_sarees.js" strategy="afterInteractive" />
-                            </details>
+                            </section>
+                            <Script src="/products/cotton_sarees.js" strategy="afterInteractive" />
+
                         </li>
                         <li>
-                            <details>
-                                <summary>Cotton Silk</summary>
-                                <section className="text-gray-600 body-font">
-                                    <div className="container px-5 py-24 mx-auto">
-                                        <div id="product-container-cotton_silk" className="flex flex-wrap -m-4">
 
-                                        </div>
+                            {/* Cotton Silk */}
+                            <section className="text-gray-600 body-font">
+                                <div className="container px-5 py-24 mx-auto">
+                                    <div id="product-container-cotton_silk" className="flex flex-wrap -m-4">
+
                                     </div>
+                                </div>
 
-                                </section>
-                                <Script src="/products/cotton_silk.js" strategy="afterInteractive" />
-                            </details>
+                            </section>
+                            <Script src="/products/cotton_silk.js" strategy="afterInteractive" />
+
                         </li>
                         <li>
-                            <details>
-                                <summary>Art Silk</summary>
-                                <section className="text-gray-600 body-font">
-                                    <div className="container px-5 py-24 mx-auto">
-                                        <div id="product-container-art_silk" className="flex flex-wrap -m-4">
 
-                                        </div>
+                            {/* Art Silk */}
+                            <section className="text-gray-600 body-font">
+                                <div className="container px-5 py-24 mx-auto">
+                                    <div id="product-container-art_silk" className="flex flex-wrap -m-4">
+
                                     </div>
+                                </div>
 
-                                </section>
-                                <Script src="/products/art_silk.js" strategy="afterInteractive" />
-                            </details>
+                            </section>
+                            <Script src="/products/art_silk.js" strategy="afterInteractive" />
+
                         </li>
                         <li>
-                            <details>
-                                <summary>Desiginer Sarees</summary>
-                                <section className="text-gray-600 body-font">
-                                    <div className="container px-5 py-24 mx-auto">
-                                        <div id="product-container-desiginer" className="flex flex-wrap -m-4">
 
-                                        </div>
+                            {/* Desiginer Sarees */}
+                            <section className="text-gray-600 body-font">
+                                <div className="container px-5 py-24 mx-auto">
+                                    <div id="product-container-desiginer" className="flex flex-wrap -m-4">
+
                                     </div>
+                                </div>
 
-                                </section>
-                                <Script src="/products/desiginer_sarees.js" strategy="afterInteractive" />
-                            </details>
+                            </section>
+                            <Script src="/products/desiginer_sarees.js" strategy="afterInteractive" />
+
                         </li>
                         <li>
-                            <details>
-                                <summary>khun sarees (खण)</summary>
-                                <section className="text-gray-600 body-font">
-                                    <div className="container px-5 py-24 mx-auto">
-                                        <div id="product-container-khun" className="flex flex-wrap -m-4">
 
-                                        </div>
+                            {/* khun sarees (खण) */}
+                            <section className="text-gray-600 body-font">
+                                <div className="container px-5 py-24 mx-auto">
+                                    <div id="product-container-khun" className="flex flex-wrap -m-4">
+
                                     </div>
+                                </div>
 
-                                </section>
-                                <Script src="/products/khun_sarees.js" strategy="afterInteractive" />
-                            </details>
+                            </section>
+                            <Script src="/products/khun_sarees.js" strategy="afterInteractive" />
+
                         </li>
                         <li>
-                            <details>
-                                <summary>Net Sarees</summary>
-                                <section className="text-gray-600 body-font">
-                                    <div className="container px-5 py-24 mx-auto">
-                                        <div id="product-container-net" className="flex flex-wrap -m-4">
 
-                                        </div>
+                            {/* Net Sarees */}
+                            <section className="text-gray-600 body-font">
+                                <div className="container px-5 py-24 mx-auto">
+                                    <div id="product-container-net" className="flex flex-wrap -m-4">
+
                                     </div>
+                                </div>
 
-                                </section>
-                                <Script src="/products/net_sarees.js" strategy="afterInteractive" />
-                            </details>
+                            </section>
+                            <Script src="/products/net_sarees.js" strategy="afterInteractive" />
+
                         </li>
-                      <li>
-                        <details>
-                            <summary>Dola Silk</summary>
+                        <li>
+
+                            {/* Dola Silk */}
                             <section className="text-gray-600 body-font">
                                 <div className="container px-5 py-24 mx-auto">
                                     <div id="product-container-dola_silk" className="flex flex-wrap -m-4">
@@ -121,23 +121,23 @@ export default function ShopClient() {
                                 </div>
                             </section>
                             <Script src="/products/dola_silk.js" strategy="afterInteractive" />
-                        </details>
-                      </li>
+
+                        </li>
                         <li>
-                            <details>
-                                <summary>Wedding Sarees</summary>
-                                <section className="text-gray-600 body-font">
-                                    <div className="container px-5 py-24 mx-auto">
-                                        <div id="product-container-wedding" className="flex flex-wrap -m-4">
 
-                                        </div>
+                            {/* Wedding Sarees */}
+                            <section className="text-gray-600 body-font">
+                                <div className="container px-5 py-24 mx-auto">
+                                    <div id="product-container-wedding" className="flex flex-wrap -m-4">
+
                                     </div>
+                                </div>
 
-                                </section>
-                                <Script src="/products/wedding_sarees.js" strategy="afterInteractive" />
-                                
-                                <a href="#top" className="bg-blue-500 text-[15px] p-3 text-white rounded-lg items-center justify-center flex w-[150px] text-center mx-auto">Scroll To Top</a>
-                            </details>
+                            </section>
+                            <Script src="/products/wedding_sarees.js" strategy="afterInteractive" />
+
+                            <a href="#top" className="bg-blue-500 text-[15px] p-3 text-white rounded-lg items-center justify-center flex w-[150px] text-center mx-auto">Scroll To Top</a>
+
                         </li>
                     </ul>
                 </section>
